4f0986b4bd6f6a576f57a271a93c5b687ba8cf02
Ville M. Vainio <ville.vainio@basware.com>
2015-05-26 20:10:38 +0300

    start from projectscaffold

---
+++ src/fndiff/AssemblyInfo.fs
@@ -0,0 +1,12 @@
+﻿namespace System
+open System.Reflection
+
+[<assembly: AssemblyTitleAttribute("fndiff")>]
+[<assembly: AssemblyProductAttribute("fndiff")>]
+[<assembly: AssemblyDescriptionAttribute("Merge by function")>]
+[<assembly: AssemblyVersionAttribute("1.0")>]
+[<assembly: AssemblyFileVersionAttribute("1.0")>]
+do ()
+
+module internal AssemblyVersionInformation =
+    let [<Literal>] Version = "1.0"
---
+++ tests/fndiff.Tests/Tests.fs
@@ -0,0 +1,10 @@
+module fndiff.Tests
+
+open fndiff
+open NUnit.Framework
+
+[<Test>]
+let ``hello returns 42`` () =
+  let result = Library.hello 42
+  printfn "%i" result
+  Assert.AreEqual(42,result)
